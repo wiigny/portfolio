46ae443aa4f82cef2286b1ea3ad5e0d9a4da7b33
wiigny <wigny_36@hotmail.com>
2023-07-14 16:52:19 -0300

    feat: including more stack

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -2,30 +2,25 @@ import { SiPostgresql, SiTypescript } from "react-icons/si";
 import { FaPython } from "react-icons/fa";
 import { FaHtml5, FaCss3Alt, FaJs, FaNode, FaReact } from "react-icons/fa";
 import { DiDjango } from "react-icons/di";
+import {
+  SiExpress,
+  SiTailwindcss,
+  SiStyledcomponents,
+  SiNextdotjs,
+} from "react-icons/si";
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: FaHtml5,
-  },
-  {
-    title: "CSS",
-    img: FaCss3Alt,
-  },
-  {
-    title: "JS",
-    img: FaJs,
-  },
+  { title: "HTML", img: FaHtml5 },
+  { title: "CSS", img: FaCss3Alt },
+  { title: "JS", img: FaJs },
   { title: "TypeScript", img: SiTypescript },
-  {
-    title: "Node JS",
-    img: FaNode,
-  },
-  {
-    title: "React",
-    img: FaReact,
-  },
+  { title: "Node JS", img: FaNode },
+  { title: "Express", img: SiExpress },
+  { title: "React", img: FaReact },
+  { title: "Styled Components", img: SiStyledcomponents },
   { title: "PostgreSQL", img: SiPostgresql },
   { title: "Python", img: FaPython },
   { title: "Django", img: DiDjango },
+  { title: "Tailwind", img: SiTailwindcss },
+  { title: "Next Js", img: SiNextdotjs },
 ];
